d01107e1c79312e5ddddb82ebc51ccd6b1831585
Alfread Rorw <alfreadrorw@gmail.com>
2026-01-25 18:34:28 +0800

    upfate

--- main.lua
+++ main.lua
@@ -666,7 +666,7 @@ local FeatureList = {
     
     --Utilityyy
     {category = "Utility", name = "fruitFarm", url = GitHubBaseURL .. "features/Utility/fruitFarm.lua"},
-    {category = "Combat", name = "sword", url = GitHubBaseURL .. "features/Combat/sword.lua"},
+    {category = "Combat", name = "Battlegrounds", url = GitHubBaseURL .. "features/Combat/Battlegrounds.lua"},
 
     -- Recording
     {category = "Record", name = "recording", url = GitHubBaseURL .. "features/Record/recording.lua"},
