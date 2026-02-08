b81ff1f157443ca06129e4b436bdee7e596f78ad
alfreadrorw1 <alfreadrorw@gmail.com>
2026-02-08 16:35:32 +0800

    update

--- main.lua
+++ main.lua
@@ -661,7 +661,7 @@ local FeatureList = {
     
     -- Visual Features
     {category = "Visual", name = "esp", url = GitHubBaseURL .. "features/Visual/esp.lua"},
-    {category = "Utility", name = "Jump&analog", url = GitHubBaseURL .. "features/Utility/Jump&analog.lua"},
+    {category = "Visual", name = "cloneAvatar", url = GitHubBaseURL .. "features/Visual/cloneAvatar.lua"},
     
     -- Aura Features
     {category = "Aura", name = "aura", url = GitHubBaseURL .. "features/Aura/aura.lua"},
